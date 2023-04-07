bbad1e7383d4c9308dc5a2fad71b48cca7a123e7
Roman Kukla <kukla.roman@gmail.com>
2023-04-07 23:55:51 +0300

    Added second test

--- src/test/java/com/rkukla/SumCalculatorTest.java
+++ src/test/java/com/rkukla/SumCalculatorTest.java
@@ -18,4 +18,12 @@ class SumCalculatorTest {
         int expected = 1;
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    public void testThatSumCalculatorWorksOkWith3(){
+        int actual = sumCalculator.sum(3);
+        int expected = 6;
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
